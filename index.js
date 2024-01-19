1522212d9eba138261861becd731835b966a8fbf
soniogoo <156035072+soniogoo@users.noreply.github.com>
2024-01-19 10:43:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = ["ꜱᴏʟᴀʀɴᴇᴛᴡᴏʀᴋ.ɪᴛ"];
 
 
 let currentIndex = 0;
